92eb8db3fb5121cd9f3f30192e00b049a6916f77
erfan <erfanghezi5050@gmail.com>
2026-04-27 23:28:37 +0330

    Test commit ...

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,7 +3,7 @@ export default function Page({ params: { id } }: { params: { id: string } }) {
     <section className="mt-44 w-[95%] mx-auto">
       sdgsdg
       <div className="flex items-center justify-center bg-white h-[20rem] mt-5 rounded-xl">
-        
+        hello we are in prj
       </div>
     </section>
   );
